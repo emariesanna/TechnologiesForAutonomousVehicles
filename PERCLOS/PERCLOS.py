28a796a6a47e6e99f9a4bc6eb4508eb4937ee58d
Marco-gh <marcosilveri32@gmail.com>
2025-04-16 13:05:56 +0200

    Ciaooo

--- PERCLOS/PERCLOS.py
+++ PERCLOS/PERCLOS.py
@@ -30,6 +30,10 @@ mp_drawing = mp.solutions.drawing_utils
 
 drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
+# Funzione per calcolare la distanza euclidea tra due punti
+def distanza_euclidea(p1, p2):
+    return np.linalg.norm(np.array(p1) - np.array(p2))
+
 # 3 - Open the video source
 cap = cv2.VideoCapture(0)  # Local webcam (index start from 0)
 
@@ -41,11 +45,13 @@ punti_EAR_DX = {160:None,158:None,153:None,163:None,33:None,133:None}
 punti_EAR_SX = {263:None,384:None,387:None,373:None,381:None,362:None}
 
 # Variabili per calcolare PERCLOS
+i1, i2, i3, i4 = 0, 0, 0, 0
 t1, t2, t3, t4 = 0, 0, 0, 0
-is_eye_open = False
+eye_state = -1
 perc_time = 0
-threshold_80 = 0.19 # Soglie a caso
-threshold_20 = 0.10
+threshold_80 = 0.18 # Soglie più o meno a caso
+threshold_20 = 0.13
+
 
 # 4 - Iterate (within an infinite loop)
 while cap.isOpened():
@@ -92,38 +98,64 @@ while cap.isOpened():
 
         end = time.time()
         totalTime = end - start
+        fps = 1 / totalTime if totalTime > 0 else 0
 
-        if totalTime > 0:
-            fps = 1 / totalTime
-        else:
-            fps = 0
+        # Calcolo EAR con distanza euclidea
+        EAR_DX = (
+            distanza_euclidea(punti_EAR_DX[160], punti_EAR_DX[163]) +
+            distanza_euclidea(punti_EAR_DX[158], punti_EAR_DX[153])
+        ) / (
+            2 * distanza_euclidea(punti_EAR_DX[33], punti_EAR_DX[133])
+        )
+        EAR_SX = (
+            distanza_euclidea(punti_EAR_SX[384], punti_EAR_SX[381]) +
+            distanza_euclidea(punti_EAR_SX[387], punti_EAR_SX[373])
+        ) / (
+            2 * distanza_euclidea(punti_EAR_SX[362], punti_EAR_SX[263])
+        )
 
-        # Calcolo EAR
-        EAR_DX = (abs(punti_EAR_DX[160][1] - punti_EAR_DX[163][1]) + abs(punti_EAR_DX[158][1] - punti_EAR_DX[153][1])) / (2 * abs(punti_EAR_DX[33][0] - punti_EAR_DX[133][0]))
-        EAR_SX = (abs(punti_EAR_SX[384][1] - punti_EAR_SX[381][1]) + abs(punti_EAR_SX[387][1] - punti_EAR_SX[373][1])) / (2 * abs(punti_EAR_SX[362][0] - punti_EAR_SX[263][0]))
-
-        cv2.putText(image, f'EAR DX : {float(EAR_DX)}', (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 2)
-        cv2.putText(image, f'EAR SX : {EAR_SX}', (20, 90), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 2)
+        cv2.putText(image, f'EAR DX : {float(EAR_DX):.3f}', (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 2)
+        cv2.putText(image, f'EAR SX : {float(EAR_SX):.3f}', (20, 90), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 2)
 
         # Calcolo PERCLOS
-        if EAR_DX > threshold_80 and EAR_SX > threshold_80:
-            if t1 == 0:
-                t1 = time.time()
-            is_eye_open = True
-
-        if EAR_DX < threshold_20 and EAR_SX < threshold_20:
-            if t2 == 0:
-                t2 = time.time()
-            is_eye_open = False
-
-        if not is_eye_open and t2 > 0:
-            t3 = time.time()
-        if is_eye_open and t1 > 0:
-            t4 = time.time()
+        if EAR_DX >= threshold_80 and EAR_SX >= threshold_80:
+            if eye_state == 4 or eye_state == -1:
+                print(f"eye state, from {str(eye_state)} to 1")
+                eye_state = 1
+                i1 = time.time()
+                print("1: "+str(t1))
+                if i1 > 0 and i4 > 0:
+                    t4 = i4 - i2
+        elif (EAR_DX < threshold_80 and EAR_DX > threshold_20) and (EAR_SX < threshold_80 and EAR_SX > threshold_20):
+            if eye_state == 1:
+                print(f"eye state, from {str(eye_state)} to 2")
+                eye_state = 2
+                i2 = time.time()
+                print("i2: "+str(i2))
+                if i1 > 0 and i2 > 0:
+                    t1 = i2 - i1
+            elif eye_state == 3:
+                print(f"eye state, from {str(eye_state)} to 4")
+                eye_state = 4
+                i4 = time.time()
+                print("i4: "+str(i4))
+                if i3 > 0 and i4 > 0:
+                    t3 = i4 - i3
+        else: #elif EAR_DX <= threshold_20 and EAR_SX <= threshold_20:
+            if eye_state == 2:
+                print(f"eye state, from {str(eye_state)} to 3")
+                eye_state = 3
+                i3 = time.time()
+                print("i3: "+str(i3))
+                if i3 > 0 and i2 > 0:
+                    t2 = i3 - i2
 
         if t1 > 0 and t2 > 0 and t3 > 0 and t4 > 0:
             perc_time = (t3 - t2) / (t4 - t1)
             cv2.putText(image, f'PERCLOS : {perc_time:.2f}', (20, 140), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 2)
+
+        if t1 > 2000:
+            cv2.putText(image, f'SBATTI LE PALPEBRE SONO PREOCCUPATO', (60, 140), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 2)
 
         # Show FPS
         cv2.putText(image, f'FPS : {int(fps)}', (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 2)
@@ -136,6 +168,3 @@ while cap.isOpened():
 
 # 5 - Close properly source and eventual log file
 cap.release()
-#log_file.close()
-    
-# [EOF]
